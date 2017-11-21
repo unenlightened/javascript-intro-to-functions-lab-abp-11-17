ac5bc948d2adab7fb0ff85729889df0b2c98bef3
Chely Ho <adept_inept@yahoo.com>
2017-11-21 08:22:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ function logWhisper(string){
   console.log(whisper(string));
   return string;
 }
+
+function sayHiToGrandma(string){
+  switch(true){
+    case string === string.toLowerCase():
+      return "I can't hear you!";
+    case string === string.toUpperCase():
+      return "YES INDEED!";
+    case string === "I love you, Grandma.":
+      return "I love you, too.";
+    default:
+      return console.log("You were ignored. The silent treatment. Or she seriously didn't notice you...");
+  }
